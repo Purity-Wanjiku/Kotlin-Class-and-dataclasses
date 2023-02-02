72351997980c640926f3ce6d06a6c6169ed00ebd
Purity-Wanjiku <ndiranguwanjikupurity@gmail.com>
2023-02-02 16:35:50 +0300

    Create student record

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    var fullname = "purity"
+    var age = "23"
+    var phoneNumber = "0771802224"
+    var weightInKg = "45"
+    val kenyan: Boolean = true
+    val notKenyan: Boolean = false
+
+
+
+
+
+
+
+
+
+
+}
